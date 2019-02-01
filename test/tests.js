2361a70518bf1560a1ec1b8467b894e9635a19b2
rickhallett <richard.hallett@ims-evolve.com>
2019-02-01 21:45:56 +0000

    added some more tests, seperated some into a FileReader class

--- test/tests.js
+++ test/tests.js
@@ -2,11 +2,27 @@
 const monsters = require('../index');
 const { expect } = require('chai');
 
-describe('Game', function() {
+describe('FileProcessor', function() {
 
     it('can read in a world map file', function() {
         expect.fail();
     });
+
+    it('can parse the file into a javascript object', function() {
+        expect.fail();
+    });
+
+    it('it can pass this object to the WorldMap instance', function() {
+        expect.fail();
+    });
+
+    it('it should print out whatever is left of the world in the same format as the input file', function() {
+        expect.fail();
+    });
+
+});
+
+describe('Game', function() {
 
     it('can read in a command line argument and store this', function() {
         expect.fail();
@@ -23,7 +39,7 @@ describe('Game', function() {
     it('places monsters randomly on the world map', function() {
         expect.fail();
     });
-    
+
     it('terminates if all the monsters have been destroyed', function() {
         expect.fail();
     });
@@ -36,6 +52,10 @@ describe('Game', function() {
 
 describe('WorldMap', function() {
 
+    it('can create a JavaScript object representing the file', function() {
+        expect.fail();
+    });
+
     it('has a city for every line of the read in file', function() {
         expect.fail();
     });
@@ -44,48 +64,44 @@ describe('WorldMap', function() {
         expect.fail();
     });
 
-    // it('', function() {
-    //     expect.fail();
-    // });
-
 });
 
 describe('City', function() {
 
     it('has an assigned name', function() {
-
+        expect.fail();
     });
 
     it('has key-value links to other cities', function() {
-
+        expect.fail();
     });
 
     it('is aware if a monster is occupying it', function() {
-
+        expect.fail();
     });
 
     it('is destroyed if two monsters are occupying it', function() {
-
+        expect.fail();
     });
 
 });
 
-describe('Monsters', function() {
+describe('Monster', function() {
 
-    it('are assigned a unique identifier', function() {
-
+    it('each is assigned a unique identifier', function() {
+        expect.fail();
     });
 
-    it('are assigned names', function() {
-
+    it('each is assigned a name', function() {
+        expect.fail();
     });
 
-    it('move in a random direction each turn', function() {
-
+    it('can move in a random direction each turn', function() {
+        expect.fail();
     });
 
     it('can be trapped and unable to move', function() {
-
+        expect.fail();
     });
 
 });
