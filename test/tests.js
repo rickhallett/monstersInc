acec898937bc0b798c6f11c202be2da326e33eaa
rickhallett <richard.hallett@ims-evolve.com>
2019-02-01 17:46:24 +0000

    set up package.json, dependencies, basic file structure, and began outlining concept tests

---
+++ test/tests.js
@@ -0,0 +1,67 @@
+
+const monsters = require('../index');
+const { expect } = require('chai');
+
+describe('Game', function() {
+
+    describe('can read in a world map file', function() {
+        it('', function() {
+
+        });
+    });
+
+    describe('', function() {
+        it('', function() {
+
+        });
+    });
+
+});
+
+describe('WorldMap', function() {
+
+    describe('', function() {
+        it('', function() {
+
+        });
+    });
+
+    describe('', function() {
+        it('', function() {
+
+        });
+    });
+
+});
+
+describe('City', function() {
+
+    describe('', function() {
+        it('', function() {
+
+        });
+    });
+
+    describe('', function() {
+        it('', function() {
+
+        });
+    });
+
+});
+
+describe('Monsters', function() {
+
+    describe('', function() {
+        it('', function() {
+
+        });
+    });
+
+    describe('', function() {
+        it('', function() {
+
+        });
+    });
+
+});
