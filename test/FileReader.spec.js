c652d9402b9a63c0c7c17c09317fbffa09e82bba
rickhallett <richard.hallett@ims-evolve.com>
2019-02-02 08:42:36 +0000

    added done() callback to mocha tests to ensure correct async handlers

--- test/FileReader.spec.js
+++ test/FileReader.spec.js
@@ -17,34 +17,48 @@ describe('FileProcessor', function() {
         expect(aFileProcessorInstance).to.be.an.instanceOf(FileProcessor);
     });
 
-    it('can read in a world map file', function() {
+    it('can read in a world map file', function(done) {
         const rawData = fileProcessor.readInFile();
         rawData.then(res => {
             expect(res).to.be.an.instanceOf(Buffer);
-        });
-    });
-
-    it('converts the read file into a string that acceptable to parser', function() {
-        const rawData = fileProcessor.readInFile();
-        rawData.then(res => {
-            const acceptableString = fileProcessor.convertBufferToString(res);
-            expect(acceptableString).to.equal(smallWorldMapString);
+            done()
         }).catch(err => {
             console.log(err)
         });
     });
 
-    it('it can parse the map string into lines representing cities', function() {
+    it('converts the read file into a string that acceptable to parser', function(done) {
+        const rawData = fileProcessor.readInFile();
+        rawData.then(res => {
+            const acceptableString = fileProcessor.convertBufferToString(res);
+            expect(acceptableString).to.equal(smallWorldMapString);
+            done();
+        }).catch(err => {
+            console.log(err)
+        });
+    });
+
+    it('it can parse the map string into lines representing cities', function(done) {
         const firstCity = 'E north=Mu south=Aninige east=Dimilu west=Asmismu';
         fileProcessor.init()
             .then(() => {
                 expect(fileProcessor.fileLines).to.be.a('array');
                 expect(fileProcessor.fileLines[0]).to.equal(firstCity);
+                done()
+            }).catch(err => {
+                console.log(err)
             })
     });
 
-    xit('it can pass this to the WorldMap instance', function() {
-        expect.fail();
+    it('it can pass this to the WorldMap instance', function(done) {
+        fileProcessor.init()
+            .then(() => {
+                const lines = fileProcessor.getFileLines();
+                expect(lines).to.be.a('array');
+                done();
+            }).catch(err => {
+                console.log(err)
+            })
     });
 
     xit('it should print out whatever is left of the world in the same format as the input file', function() {
