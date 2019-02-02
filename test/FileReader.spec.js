996999320303540d94753a0e1207cadf89a69a08
rickhallett <richard.hallett@ims-evolve.com>
2019-02-02 08:27:19 +0000

    it can parse the map string into lines representing cities

--- test/FileReader.spec.js
+++ test/FileReader.spec.js
@@ -1,18 +1,19 @@
 const { expect } = require('chai');
 const FileProcessor = require('../lib/FileProcessor');
-const { smallWorldMapString } = require('../constants');
+const { smallWorldMapString, rootDir } = require('../constants');
+const smallWorldMapFile = `${rootDir}/world_map_small.txt`;
 
 describe('FileProcessor', function() {
 
     let fileProcessor;
 
     beforeEach(function(){
-        fileProcessor = new FileProcessor();
+        fileProcessor = new FileProcessor(smallWorldMapFile);
     });
 
     it('exports a FileProcessor constructor', function() {
         const FileProcessor = require('../lib/FileProcessor');
-        const aFileProcessorInstance = new FileProcessor();
+        const aFileProcessorInstance = new FileProcessor(smallWorldMapFile);
         expect(aFileProcessorInstance).to.be.an.instanceOf(FileProcessor);
     });
 
@@ -33,15 +34,20 @@ describe('FileProcessor', function() {
         });
     });
 
-    it('can parse the file into a javascript object', function() {
+    it('it can parse the map string into lines representing cities', function() {
+        const firstCity = 'E north=Mu south=Aninige east=Dimilu west=Asmismu';
+        fileProcessor.init()
+            .then(() => {
+                expect(fileProcessor.fileLines).to.be.a('array');
+                expect(fileProcessor.fileLines[0]).to.equal(firstCity);
+            })
+    });
+
+    xit('it can pass this to the WorldMap instance', function() {
         expect.fail();
     });
 
-    it('it can pass this object to the WorldMap instance', function() {
-        expect.fail();
-    });
-
-    it('it should print out whatever is left of the world in the same format as the input file', function() {
+    xit('it should print out whatever is left of the world in the same format as the input file', function() {
         expect.fail();
     });
 
