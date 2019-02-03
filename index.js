75f168e40f8bad63e59f129d1ed921d83aecbfa9
rickhallett <richard.hallett@ims-evolve.com>
2019-02-03 21:02:38 +0000

    bugfix: node creation was occuring outside forEach scope and so retaining previous file line values and screwing up the entire map after the first node

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const Game = require('./lib/Game');
 
 // Initialise Game Process
-const gameInstance = new Game(100);
-gameInstance.initialiseWorld();
+const gameInstance = new Game(50);
+const { executeClockCycleMove, worldMap } = gameInstance.initialiseWorld()
+    .then(res => {
+        let worldMap = res.executeClockCycleMove(res.worldMap);
+        let scannedWorldMap = worldMap.destroyOverOccupiedCities()
+        console.log(scannedWorldMap);
+    });
+
+
+
--- test/WorldMap.spec.js
+++ test/WorldMap.spec.js
@@ -94,15 +94,6 @@ describe('WorldMap', function() {
         });
     });
 
-    // TODO: how can we deep search a Set for property values?
-    xit('ignores file line entries that have duplicated cities', function(done) {
-        const game = new Game(100);
-        game.initialiseWorld(largeMapFileWithDuplicates).then((res) => {
-            expect(res.map.size).to.equal(6763);
-            done();
-        });
-    });
-
     it('limits the number of monsters initialised in each city to 1', function(done) {
         const game = new Game(100);
         game.initialiseWorld(smallMapFile, false, false).then((res) => {
@@ -144,78 +135,5 @@ describe('WorldMap', function() {
             done();
         });
     });
-
-    // TODO: debug, refactor or delete the world's most ridiculous test
-    // TODO: let this be a lesson to you Sir; do not test for something that is never going to exist in your program
-    // xit('moves every monster to a new city on each game clock cycle', function(done) {
-    //     const game = new Game(100);
-    //     game.initialiseWorld(smallMapFile, false, true).then((res) => {
-    //
-    //         const oldMap = cloneDeep(res.worldMap);
-    //         const oldResidents = [];
-    //         oldMap.map.forEach(city => {
-    //             oldResidents.push({
-    //                 cityName: city.cityName,
-    //                 occupiers: city.occupiers.map(monster => monster)
-    //             });
-    //         });
-    //
-    //         const newMap = res.initialiseClockCycle(res.worldMap);
-    //         const newResidents = [];
-    //         newMap.map.forEach(city => {
-    //             newResidents.push({
-    //                 cityName: city.cityName,
-    //                 occupiers: city.occupiers.map(monster => monster)
-    //             });
-    //         });
-    //
-    //         let anIdentical = false;
-    //         oldResidents.forEach((resident, index) => {
-    //             if (resident.cityName === newResidents[index].cityName) {
-    //
-    //                 function findLongerOccupiersArray() {
-    //                     const oldOccupierQuantity = resident.occupiers.length;
-    //                     const newOccupierQuantity = newResidents[index].occupiers.length;
-    //                     const largest = oldOccupierQuantity > newOccupierQuantity ? oldOccupierQuantity : newOccupierQuantity;
-    //                     return largest;
-    //                 }
-    //
-    //                 function residentID(i){
-    //                     return resident.occupiers[i] ? resident.occupiers[i].id : resident.occupiers[i]
-    //                 }
-    //
-    //                 function newResidentID(i) {
-    //                     return newResidents[index].occupiers[i] ? newResidents[index].occupiers[i].id : newResidents[index].occupiers[i]
-    //                 }
-    //
-    //                 function residentCityName() {
-    //                     return resident.cityName;
-    //                 }
-    //
-    //                 function newResidentCityName() {
-    //                     return newResidents[index].cityName;
-    //                 }
-    //
-    //                 const largest = findLongerOccupiersArray();
-    //
-    //                 for (let i = 0; i < largest; i++) {
-    //
-    //                     const isValidID = typeof residentID(i) === 'string' ? residentID(i) : 1;
-    //                     const isValidNewID = typeof newResidentID(i) === 'string' ? newResidentID(i) : 2;
-    //                     const checkName = residentCityName();
-    //                     const checkNewName = newResidentCityName();
-    //
-    //                     if(isValidID === isValidNewID && checkName === checkNewName) {
-    //                         anIdentical = true;
-    //                     }
-    //                 }
-    //             }
-    //         });
-    //
-    //         expect(anIdentical).to.equal(false);
-    //         done();
-    //
-    //     });
-    // });
 
 });
