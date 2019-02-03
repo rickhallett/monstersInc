032448bf62a67e3673a07f2d45aaf9cd8b656e2e
rickhallett <richard.hallett@ims-evolve.com>
2019-02-03 23:37:46 +0000

    added print out messages on city destruction as per spec

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const gameInstance = new Game(50);
 const { executeClockCycleMove, worldMap } = gameInstance.initialiseWorld()
     .then(res => {
         let worldMap = res.executeClockCycleMove(res.worldMap);
-        let scannedWorldMap = worldMap.destroyOverOccupiedCities()
-        console.log(scannedWorldMap);
+        let scannedWorldMap = worldMap.destroyOverOccupiedCities();
+
+        // this is fucking it. nearly there.
+        // let worldMap = res.worldMap;
+        //
+        // while (worldMap.totalMovesCompleted < 10000 && !worldMap.worldDestroyed) {
+        //     worldMap = res.executeClockCycleMove(worldMap);
+        //     worldMap = worldMap.destroyOverOccupiedCities();
+        // }
+
+
     });
 
 
