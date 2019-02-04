b71362e868f2f133a92f7b95b9a11b13de03a7a6
rickhallett <richard.hallett@ims-evolve.com>
2019-02-04 10:04:56 +0000

    remove meaningless Game tests

--- test/Game.spec.js
+++ test/Game.spec.js
@@ -16,28 +16,9 @@ describe('Game', function() {
        game = new Game(100);
     });
 
-    // TODO: find out how mocha/chai can make these tests more meaningful
-    describe('dependency injection', function() {
-        it('connects to the FileReader class through dependency injection', function() {
-            expect(game.FileProcessor).to.equal(FileProcessor)
-        });
-
-        it('connects to the WorldMap class through dependency injection', function() {
-            expect(game.WorldMap).to.equal(WorldMap);
-        });
-
-        it('connects to the Monster class through dependency injection', function() {
-            expect(game.Monster).to.equal(Monster);
-        });
-    });
-
     it('can generate N monsters', function() {
         const monsters = game.generateMonsters();
         expect(monsters.length).to.equal(100);
-    });
-
-    xit('can pass the monsters to the worldMap', function() {
-        expect.fail();
     });
 
     xit('places monsters randomly on the world map', function() {
