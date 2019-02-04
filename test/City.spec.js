54b1be58111cc38ab299b1094ac9a3ede915dc84
rickhallett <richard.hallett@ims-evolve.com>
2019-02-04 08:37:31 +0000

    new test passing: 'has an assigned name'

--- test/City.spec.js
+++ test/City.spec.js
@@ -1,21 +1,37 @@
 const { it } = require('mocha');
 const { expect } = require('chai');
+const FileProcessor = require('../lib/FileProcessor');
+const WorldMap = require('../lib/WorldMap');
+const constants = require('../constants');
+const smallMapFile = `${constants.rootDir}/world_map_small.txt`;
 
-xdescribe('City', function() {
+const firstLineOfSmallWorldMapFile = 'E north=Mu south=Aninige east=Dimilu west=Asmismu';
 
-    it('has an assigned name', function() {
+describe('City', function() {
+
+    it('has an assigned name', function(done) {
+        const fileProcessor = new FileProcessor(smallMapFile);
+        const worldMap = new WorldMap();
+        fileProcessor.init().then(res => {
+            const fileLines = res;
+            const constructedMap = worldMap.constructMap(fileLines);
+            const firstExpectedCityName = 'E';
+            const firstExpectedCity = constructedMap.get(firstExpectedCityName);
+
+            expect(firstExpectedCity.cityName).to.equal('E');
+            done();
+        });
+    });
+
+    xit('has key-value links to other cities', function() {
         expect.fail();
     });
 
-    it('has key-value links to other cities', function() {
+    xit('is aware if a monster is occupying it', function() {
         expect.fail();
     });
 
-    it('is aware if a monster is occupying it', function() {
-        expect.fail();
-    });
-
-    it('is destroyed if two monsters are occupying it', function() {
+    xit('is destroyed if two monsters are occupying it', function() {
         expect.fail();
     });
 
